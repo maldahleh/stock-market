c51d13393f37a56e17228838c79571558846582b
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-10-24 21:59:06 -0400

    Improves database storage

--- src/main/java/com/maldahleh/stockmarket/storage/Storage.java
+++ src/main/java/com/maldahleh/stockmarket/storage/Storage.java
@@ -1,22 +1,175 @@
 package com.maldahleh.stockmarket.storage;
 
 import com.maldahleh.stockmarket.transactions.Transaction;
+import com.maldahleh.stockmarket.transactions.types.TransactionType;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import org.bukkit.configuration.ConfigurationSection;
 
-public interface Storage {
+public abstract class Storage extends StorageStatements {
 
-  int getNextId();
+  private final HikariDataSource pool;
+  private int currentId;
 
-  void processPurchase(Transaction transaction);
+  protected abstract HikariConfig buildHikariConfig(ConfigurationSection section);
 
-  void processSale(Transaction transaction);
+  protected abstract Transaction buildTransaction(ResultSet resultSet) throws SQLException;
 
-  void markSold(Transaction transaction);
+  protected Storage(ConfigurationSection section) {
+    pool = new HikariDataSource(buildHikariConfig(section));
+    createTable();
 
-  List<Transaction> getPlayerTransactions(UUID uuid);
+    currentId = getLastId();
+  }
 
-  List<Transaction> getStockTransactions(String symbol);
+  public int getNextId() {
+    currentId++;
+    return currentId;
+  }
 
-  List<Transaction> getTransactionHistory();
+  public void processTransaction(Transaction transaction) {
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = buildActionStatement(connection, transaction)) {
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public List<Transaction> getTransactionHistory() {
+    List<Transaction> transactions = new ArrayList<>();
+
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = connection.prepareStatement(getRecentTransactionsQuery());
+        ResultSet resultSet = statement.executeQuery()) {
+      while (resultSet.next()) {
+        transactions.add(buildTransaction(resultSet));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return transactions;
+  }
+
+  public List<Transaction> getPlayerTransactions(UUID uuid) {
+    List<Transaction> transactions = new ArrayList<>();
+
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = buildPlayerTransactionStatement(connection, uuid);
+        ResultSet resultSet = statement.executeQuery()) {
+      while (resultSet.next()) {
+        transactions.add(buildTransaction(resultSet));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return transactions;
+  }
+
+  public List<Transaction> getStockTransactions(String symbol) {
+    List<Transaction> transactions = new ArrayList<>();
+
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = buildStockTransactionStatement(connection, symbol);
+        ResultSet resultSet = statement.executeQuery()) {
+      while (resultSet.next()) {
+        transactions.add(buildTransaction(resultSet));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return transactions;
+  }
+
+  public void markSold(Transaction transaction) {
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = buildMarkSoldStatement(connection, transaction)) {
+      statement.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void createTable() {
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = connection.prepareStatement(getCreateTableQuery())) {
+      statement.execute();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private int getLastId() {
+    try (Connection connection = pool.getConnection();
+        PreparedStatement statement = connection.prepareStatement(getMaxIdQuery());
+        ResultSet resultSet = statement.executeQuery()) {
+      if (resultSet.next()) {
+        return resultSet.getInt(1);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return -1;
+  }
+
+  private PreparedStatement buildActionStatement(Connection connection, Transaction transaction)
+      throws SQLException {
+    PreparedStatement statement = connection.prepareStatement(getActionQuery(transaction));
+    statement.setInt(1, transaction.getId());
+    statement.setString(2, transaction.getUuid().toString());
+    statement.setTimestamp(3, Timestamp.from(transaction.getTransactionDate()));
+    statement.setString(4, transaction.getSymbol().toUpperCase());
+    statement.setInt(5, transaction.getQuantity());
+    statement.setString(6, transaction.getSinglePrice().toPlainString());
+    statement.setString(7, transaction.getBrokerFee().toPlainString());
+    if (transaction.getEarnings() != null) {
+      statement.setString(8, transaction.getEarnings().toPlainString());
+    }
+
+    return statement;
+  }
+
+  private String getActionQuery(Transaction transaction) {
+    if (transaction.getTransactionType() == TransactionType.SALE) {
+      return getSaleQuery();
+    } else {
+      return getPurchaseQuery();
+    }
+  }
+
+  protected PreparedStatement buildPlayerTransactionStatement(Connection connection, UUID uuid)
+      throws SQLException {
+    PreparedStatement statement = connection.prepareStatement(getPlayerTransactionsQuery());
+    statement.setString(1, uuid.toString());
+
+    return statement;
+  }
+
+  private PreparedStatement buildStockTransactionStatement(Connection connection, String symbol)
+      throws SQLException {
+    PreparedStatement statement = connection.prepareStatement(getStockTransactionsQuery());
+    statement.setString(1, symbol);
+
+    return statement;
+  }
+
+  protected PreparedStatement buildMarkSoldStatement(Connection connection, Transaction transaction)
+      throws SQLException {
+    PreparedStatement statement = connection.prepareStatement(getMarkSoldQuery());
+    statement.setInt(1, transaction.getId());
+
+    return statement;
+  }
 }
--- src/main/java/com/maldahleh/stockmarket/storage/types/SQLite.java
+++ src/main/java/com/maldahleh/stockmarket/storage/types/SQLite.java
@@ -3,267 +3,63 @@ package com.maldahleh.stockmarket.storage.types;
 import com.maldahleh.stockmarket.storage.Storage;
 import com.maldahleh.stockmarket.transactions.Transaction;
 import com.maldahleh.stockmarket.transactions.types.TransactionType;
+import com.zaxxer.hikari.HikariConfig;
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
+import org.bukkit.configuration.ConfigurationSection;
 
-public class SQLite implements Storage {
+public class SQLite extends Storage {
 
-  private static final String SQLITE_URL = "jdbc:sqlite:plugins/StockMarket/StockMarket.db";
-
-  private static final String CREATE_QUERY =
-      "CREATE TABLE IF NOT EXISTS "
-          + "sm_transactions(id INTEGER PRIMARY KEY, uuid CHAR(36), tran_type VARCHAR(8), "
-          + "tran_date DATETIME, symbol VARCHAR(12), quantity INTEGER, single_price VARCHAR(20), "
-          + "broker_fee VARCHAR(20), earnings VARCHAR(20), sold BOOLEAN)";
-  private static final String PURCHASE_QUERY =
-      "INSERT INTO sm_transactions (id, uuid, tran_type, "
-          + "tran_date, symbol, quantity, single_price, broker_fee) VALUES (?, ?, 'purchase', "
-          + "?, ?, ?, ?, ?)";
-  private static final String SALE_QUERY =
-      "INSERT INTO sm_transactions (id, uuid, tran_type, tran_date, symbol, quantity, single_price,"
-          + " broker_fee, earnings) VALUES (?, ?, 'sale', ?, ?, ?, ?, ?, ?)";
-  private static final String MARK_SOLD = "UPDATE sm_transactions SET sold = true WHERE id = ?";
-  private static final String GET_QUERY =
-      "SELECT id, tran_type, tran_date, symbol, quantity, "
-          + "single_price, broker_fee, earnings, sold FROM sm_transactions WHERE uuid = ? "
-          + "ORDER BY tran_date";
-  private static final String GET_HISTORY_QUERY =
-      "SELECT id, uuid, tran_type, tran_date, symbol, "
-          + "quantity, single_price, broker_fee, earnings, sold FROM sm_transactions ORDER BY "
-          + "tran_date LIMIT 100";
-  private static final String STOCK_QUERY =
-      "SELECT id, uuid, tran_type, tran_date, symbol, quantity, single_price, broker_fee, earnings,"
-          + " sold FROM sm_transactions WHERE symbol = ? ORDER BY tran_date";
-  private static final String GET_LAST_QUERY = "SELECT MAX(id) FROM sm_transactions";
-
-  private int currentId;
-
-  public SQLite() {
-    createTables();
-    currentId = getLastId();
-  }
-
-  private void createTables() {
-    try (Connection connection = getConnection();
-        PreparedStatement statement = connection.prepareStatement(CREATE_QUERY)) {
-      statement.execute();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private int getLastId() {
-    try (Connection connection = getConnection();
-        PreparedStatement statement = connection.prepareStatement(GET_LAST_QUERY);
-        ResultSet resultSet = statement.executeQuery()) {
-      if (resultSet.next()) {
-        return resultSet.getInt(1);
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-
-    return -1;
+  public SQLite(ConfigurationSection section) {
+    super(section);
   }
 
   @Override
-  public int getNextId() {
-    currentId++;
-    return currentId;
+  protected HikariConfig buildHikariConfig(ConfigurationSection section) {
+    HikariConfig config = new HikariConfig();
+    config.setPoolName("AuthMeSQLitePool");
+    config.setDriverClassName("org.sqlite.JDBC");
+    config.setJdbcUrl("jdbc:sqlite:plugins/StockMarket/StockMarket.db");
+    config.setConnectionTestQuery("SELECT 1");
+    config.setMaxLifetime(60000);
+    config.setIdleTimeout(45000);
+    config.setMaximumPoolSize(50);
+
+    return config;
   }
 
   @Override
-  public void processPurchase(Transaction transaction) {
-    try (Connection connection = getConnection();
-        PreparedStatement statement = getActionStatement(connection, transaction, true)) {
-      statement.executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+  protected String getCreateTableQuery() {
+    return "CREATE TABLE IF NOT EXISTS sm_transactions(id INTEGER PRIMARY KEY, uuid CHAR(36), "
+        + "tran_type VARCHAR(8), tran_date DATETIME, symbol VARCHAR(12), quantity INTEGER, "
+        + "single_price VARCHAR(20), broker_fee VARCHAR(20), earnings VARCHAR(20), sold BOOLEAN)";
   }
 
   @Override
-  public void processSale(Transaction transaction) {
-    try (Connection connection = getConnection();
-        PreparedStatement statement = getActionStatement(connection, transaction, false)) {
-      statement.executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Override
-  public void markSold(Transaction transaction) {
-    try (Connection connection = getConnection();
-        PreparedStatement statement = getMarkSoldStatement(connection, transaction)) {
-      statement.executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Override
-  public List<Transaction> getPlayerTransactions(UUID uuid) {
-    List<Transaction> transactions = new ArrayList<>();
-
-    try (Connection connection = getConnection();
-        PreparedStatement statement = getPlayerGetStatement(connection, uuid);
-        ResultSet resultSet = statement.executeQuery()) {
-      while (resultSet.next()) {
-        BigDecimal earnings = null;
-
-        String earningsString = resultSet.getString(8);
-        if (earningsString != null) {
-          earnings = new BigDecimal(earningsString);
-        }
-
-        transactions.add(
-            new Transaction(
-                resultSet.getInt(1),
-                uuid,
-                TransactionType.valueOf(resultSet.getString(2)),
-                resultSet.getTimestamp(3).toInstant(),
-                resultSet.getString(4),
-                resultSet.getInt(5),
-                new BigDecimal(resultSet.getString(6)),
-                new BigDecimal(resultSet.getString(7)),
-                earnings,
-                null,
-                null,
-                resultSet.getBoolean(9)));
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-
-    return transactions;
-  }
-
-  @Override
-  public List<Transaction> getStockTransactions(String symbol) {
-    List<Transaction> transactions = new ArrayList<>();
-
-    try (Connection connection = getConnection();
-        PreparedStatement statement = getStockGetStatement(connection, symbol);
-        ResultSet resultSet = statement.executeQuery()) {
-      while (resultSet.next()) {
-        BigDecimal earnings = null;
-
-        String earningsString = resultSet.getString(9);
-        if (earningsString != null) {
-          earnings = new BigDecimal(earningsString);
-        }
-
-        transactions.add(
-            new Transaction(
-                resultSet.getInt(1),
-                UUID.fromString(resultSet.getString(2)),
-                TransactionType.valueOf(resultSet.getString(3)),
-                resultSet.getTimestamp(4).toInstant(),
-                resultSet.getString(5),
-                resultSet.getInt(6),
-                new BigDecimal(resultSet.getString(7)),
-                new BigDecimal(resultSet.getString(8)),
-                earnings,
-                null,
-                null,
-                resultSet.getBoolean(10)));
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-
-    return transactions;
-  }
-
-  @Override
-  public List<Transaction> getTransactionHistory() {
-    List<Transaction> transactions = new ArrayList<>();
-
-    try (Connection connection = getConnection();
-        PreparedStatement statement = connection.prepareStatement(GET_HISTORY_QUERY);
-        ResultSet resultSet = statement.executeQuery()) {
-      while (resultSet.next()) {
-        BigDecimal earnings = null;
-
-        String earningsString = resultSet.getString(9);
-        if (earningsString != null) {
-          earnings = new BigDecimal(earningsString);
-        }
-
-        transactions.add(
-            new Transaction(
-                resultSet.getInt(1),
-                UUID.fromString(resultSet.getString(2)),
-                TransactionType.valueOf(resultSet.getString(3)),
-                resultSet.getTimestamp(4).toInstant(),
-                resultSet.getString(5),
-                resultSet.getInt(6),
-                new BigDecimal(resultSet.getString(7)),
-                new BigDecimal(resultSet.getString(8)),
-                earnings,
-                null,
-                null,
-                resultSet.getBoolean(10)));
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-
-    return transactions;
-  }
-
-  private PreparedStatement getActionStatement(
-      Connection connection, Transaction transaction, boolean isPurchase) throws SQLException {
-    PreparedStatement statement =
-        connection.prepareStatement(isPurchase ? PURCHASE_QUERY : SALE_QUERY);
-    statement.setInt(1, transaction.getId());
-    statement.setString(2, transaction.getUuid().toString());
-    statement.setTimestamp(3, Timestamp.from(transaction.getTransactionDate()));
-    statement.setString(4, transaction.getSymbol().toUpperCase());
-    statement.setInt(5, transaction.getQuantity());
-    statement.setString(6, transaction.getSinglePrice().toPlainString());
-    statement.setString(7, transaction.getBrokerFee().toPlainString());
-    if (transaction.getEarnings() != null) {
-      statement.setString(8, transaction.getEarnings().toPlainString());
-    }
-
-    return statement;
-  }
-
-  private PreparedStatement getMarkSoldStatement(Connection connection, Transaction transaction)
+  protected Transaction buildTransaction(ResultSet resultSet)
       throws SQLException {
-    PreparedStatement statement = connection.prepareStatement(MARK_SOLD);
-    statement.setInt(1, transaction.getId());
+    BigDecimal earnings = null;
 
-    return statement;
-  }
+    String earningsString = resultSet.getString(9);
+    if (earningsString != null) {
+      earnings = new BigDecimal(earningsString);
+    }
 
-  private PreparedStatement getPlayerGetStatement(Connection connection, UUID uuid)
-      throws SQLException {
-    PreparedStatement statement = connection.prepareStatement(GET_QUERY);
-    statement.setString(1, uuid.toString());
-
-    return statement;
-  }
-
-  private PreparedStatement getStockGetStatement(Connection connection, String symbol)
-      throws SQLException {
-    PreparedStatement statement = connection.prepareStatement(STOCK_QUERY);
-    statement.setString(1, symbol);
-
-    return statement;
-  }
-
-  private Connection getConnection() throws SQLException {
-    return DriverManager.getConnection(SQLITE_URL);
+    return new Transaction(
+        resultSet.getInt(1),
+        UUID.fromString(resultSet.getString(2)),
+        TransactionType.valueOf(resultSet.getString(3)),
+        resultSet.getTimestamp(4).toInstant(),
+        resultSet.getString(5),
+        resultSet.getInt(6),
+        new BigDecimal(resultSet.getString(7)),
+        new BigDecimal(resultSet.getString(8)),
+        earnings,
+        null,
+        null,
+        resultSet.getBoolean(10)
+    );
   }
 }
