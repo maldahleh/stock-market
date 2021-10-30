b1106c74ada72a1d78c04c3153c1c42402610c1d
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-10-29 23:11:09 -0400

    Adds transactions and tutorial command tests

--- src/test/java/com/maldahleh/stockmarket/commands/subcommand/types/menus/TutorialCommandTests.java
+++ src/test/java/com/maldahleh/stockmarket/commands/subcommand/types/menus/TutorialCommandTests.java
@@ -1,6 +1,6 @@
 package com.maldahleh.stockmarket.commands.subcommand.types.menus;
 
-import com.maldahleh.stockmarket.commands.subcommands.types.menus.ListCommand;
+import com.maldahleh.stockmarket.commands.subcommands.types.menus.TutorialCommand;
 import com.maldahleh.stockmarket.inventories.InventoryManager;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,17 +9,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-class ListCommandTests {
+class TutorialCommandTests {
 
     private InventoryManager inventoryManager;
 
-    private ListCommand command;
+    private TutorialCommand command;
 
     @BeforeEach
     void setup() {
         inventoryManager = mock(InventoryManager.class);
 
-        command = new ListCommand(inventoryManager);
+        command = new TutorialCommand(inventoryManager);
     }
 
     @Test
@@ -30,24 +30,24 @@ class ListCommandTests {
         // THEN
         assertEquals(1, command.minArgs());
         assertEquals(1, command.maxArgs());
-        assertEquals("stockmarket.list", command.requiredPerm());
-        assertEquals("list", command.commandName());
+        assertEquals("stockmarket.tutorial", command.requiredPerm());
+        assertEquals("tutorial", command.commandName());
 
         assertEquals(1, command.commandHelpKeys(player).size());
-        assertEquals("list", command.commandHelpKeys(player).get(0));
+        assertEquals("tutorial", command.commandHelpKeys(player).get(0));
     }
 
     @Test
     void openInventory() {
         // GIVEN
         Player player = mock(Player.class);
-        String[] args = new String[]{"list"};
+        String[] args = new String[]{"tutorial"};
 
         // WHEN
         command.onCommand(player, args);
 
         // THEN
         verify(inventoryManager, times(1))
-                .openListInventory(player);
+                .openTutorialInventory(player);
     }
 }
