604c70bdaba5462080c22ca45f73a291d4a534e5
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-04 08:23:57 -0400

    Improves stock provider tests

--- src/test/java/com/maldahleh/stockmarket/stocks/provider/StockProviderTests.java
+++ src/test/java/com/maldahleh/stockmarket/stocks/provider/StockProviderTests.java
@@ -1,13 +1,19 @@
 package com.maldahleh.stockmarket.stocks.provider;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 
 import com.maldahleh.stockmarket.stocks.utils.SettingsUtils;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import yahoofinance.Stock;
@@ -17,57 +23,107 @@ class StockProviderTests {
 
   private final StockProvider stockProvider = new StockProvider(SettingsUtils.buildSettings());
 
-  @Test
-  void fetchStock() {
-    // GIVEN
-    String symbol = "ba";
+  @Nested
+  @DisplayName("fetchStock")
+  class FetchStock {
 
-    try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
-      yahooFinance.when(() -> YahooFinance.get("BA", true))
-          .thenReturn(new Stock("BA"));
+    @Test
+    void success() {
+      // GIVEN
+      String symbol = "ba";
 
-      // WHEN
-      Stock stock = stockProvider.get(symbol);
+      try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
+        yahooFinance.when(() -> YahooFinance.get("BA", true))
+            .thenReturn(new Stock("BA"));
 
-      // THEN
-      assertNotNull(stock);
+        // WHEN
+        Stock stock = stockProvider.get(symbol);
+
+        // THEN
+        assertNotNull(stock);
+      }
+    }
+
+    @Test
+    void cached() {
+      // GIVEN
+      String symbol = "ba";
+
+      try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
+        yahooFinance.when(() -> YahooFinance.get("BA", true))
+            .thenReturn(new Stock("BA"));
+
+        // WHEN
+        Stock stock = stockProvider.get(symbol);
+        Stock stockTwo = stockProvider.get(symbol);
+
+        // THEN
+        assertEquals(stock, stockTwo);
+
+        yahooFinance.verify(() -> YahooFinance.get("BA", true), times(1));
+      }
+    }
+
+    @Test
+    void exception() {
+      // GIVEN
+      String symbol = "ba";
+
+      try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
+        yahooFinance.when(() -> YahooFinance.get("BA", true))
+            .thenThrow(new IOException());
+
+        // WHEN
+        Stock stock = stockProvider.get(symbol);
+
+        // THEN
+        assertNull(stock);
+      }
     }
   }
 
-  @Test
-  void fetchStockCached() {
-    // GIVEN
-    String symbol = "ba";
+  @Nested
+  @DisplayName("fetchStockList")
+  class FetchStockList {
 
-    try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
-      yahooFinance.when(() -> YahooFinance.get("BA", true))
-          .thenReturn(new Stock("BA"));
+    @Test
+    void success() {
+      // GIVEN
+      String[] symbols = {"ba", "aapl", "fb"};
 
-      // WHEN
-      Stock stock = stockProvider.get(symbol);
-      Stock stockTwo = stockProvider.get(symbol);
+      try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
+        Map<String, Stock> returnMap = new HashMap<>();
+        returnMap.put("BA", new Stock("BA"));
+        returnMap.put("AAPL", null);
+        returnMap.put("FB", new Stock("FB"));
 
-      // THEN
-      assertEquals(stock, stockTwo);
+        yahooFinance.when(() -> YahooFinance.get(new String[]{"BA", "AAPL", "FB"}))
+            .thenReturn(returnMap);
 
-      yahooFinance.verify(() -> YahooFinance.get("BA", true), times(1));
+        // WHEN
+        Map<String, Stock> stocks = stockProvider.get(symbols);
+
+        // THEN
+        assertEquals(2, stocks.size());
+        assertFalse(stocks.containsKey("AAPL"));
+      }
     }
-  }
 
-  @Test
-  void fetchStockException() {
-    // GIVEN
-    String symbol = "ba";
+    @Test
+    void exception() {
+      // GIVEN
+      String[] symbols = {"ba", "aapl", "fb"};
 
-    try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
-      yahooFinance.when(() -> YahooFinance.get("BA", true))
-          .thenThrow(new IOException());
+      try (MockedStatic<YahooFinance> yahooFinance = mockStatic(YahooFinance.class)) {
+        yahooFinance.when(() -> YahooFinance.get(new String[]{"BA", "AAPL", "FB"}))
+            .thenThrow(new IOException());
 
-      // WHEN
-      Stock stock = stockProvider.get(symbol);
+        // WHEN
+        Map<String, Stock> stocks = stockProvider.get(symbols);
 
-      // THEN
-      assertNull(stock);
+        // THEN
+        assertTrue(stocks.isEmpty());
+      }
     }
   }
 }
