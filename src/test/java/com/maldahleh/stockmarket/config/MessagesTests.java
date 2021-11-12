32227b1efb7133a565012009f3790c3ceb0442c0
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-12 07:48:35 -0500

    Adds command disabled test

---
+++ src/test/java/com/maldahleh/stockmarket/config/MessagesTests.java
@@ -0,0 +1,46 @@
+package com.maldahleh.stockmarket.config;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.maldahleh.stockmarket.StockMarket;
+import java.io.File;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MessagesTests {
+
+  private Settings settings;
+  private Messages messages;
+
+  @BeforeEach
+  void setup() {
+    this.settings = mock(Settings.class);
+    this.messages = new Messages(mockedStockMarket(), settings);
+  }
+
+  @Test
+  void commandsDisabled() {
+    // GIVEN
+    Player player = mock(Player.class);
+
+    // WHEN
+    messages.sendCommandsDisabled(player);
+
+    // THEN
+    verify(player)
+        .sendMessage(ChatColor.translateAlternateColorCodes('&', "&cCommands are disabled, please use the stock brokers."));
+  }
+
+  private StockMarket mockedStockMarket() {
+    StockMarket stockMarket = mock(StockMarket.class);
+
+    when(stockMarket.getDataFolder())
+        .thenReturn(new File("src/test/resources"));
+
+    return stockMarket;
+  }
+}
