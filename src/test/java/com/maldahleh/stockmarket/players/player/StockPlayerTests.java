5702f84a0ce23a8461d176a37ecf847dac51a12c
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-13 13:41:10 -0500

    Utilizes new TransactionUtils class

--- src/test/java/com/maldahleh/stockmarket/players/player/StockPlayerTests.java
+++ src/test/java/com/maldahleh/stockmarket/players/player/StockPlayerTests.java
@@ -3,6 +3,7 @@ package com.maldahleh.stockmarket.players.player;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.maldahleh.stockmarket.players.utils.TransactionUtils;
 import com.maldahleh.stockmarket.transactions.Transaction;
 import com.maldahleh.stockmarket.transactions.types.TransactionType;
 import java.math.BigDecimal;
@@ -14,8 +15,9 @@ class StockPlayerTests {
   @Test
   void performTransactions() {
     // GIVEN
-    Transaction purchase = buildTransaction(TransactionType.PURCHASE, 3, BigDecimal.TEN);
-    Transaction sale = buildTransaction(TransactionType.SALE, 2, BigDecimal.ONE);
+    Transaction purchase = TransactionUtils.buildTransaction(TransactionType.PURCHASE, 3,
+        BigDecimal.TEN);
+    Transaction sale = TransactionUtils.buildTransaction(TransactionType.SALE, 2, BigDecimal.ONE);
 
     StockPlayer stockPlayer = new StockPlayer();
 
@@ -33,15 +35,5 @@ class StockPlayerTests {
     assertEquals(2, stockPlayer.getTransactionMap().size());
     assertEquals(1, stockPlayer.getStockMap().get("BA").getQuantity());
     assertEquals(BigDecimal.valueOf(9), stockPlayer.getStockMap().get("BA").getValue());
-  }
-
-  private Transaction buildTransaction(TransactionType type, int quantity, BigDecimal stockValue) {
-    return Transaction.builder()
-        .symbol("BA")
-        .transactionType(type)
-        .transactionDate(Instant.now())
-        .quantity(quantity)
-        .stockValue(stockValue)
-        .build();
   }
 }
