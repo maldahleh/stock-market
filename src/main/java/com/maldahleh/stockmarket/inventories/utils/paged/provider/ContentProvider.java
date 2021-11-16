11c3c3eebdef7617cbad54b2feed21bef2098648
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-16 17:00:09 -0500

    Removes redundant interface

--- src/main/java/com/maldahleh/stockmarket/inventories/utils/paged/provider/ContentProvider.java
+++ src/main/java/com/maldahleh/stockmarket/inventories/utils/paged/provider/ContentProvider.java
@@ -12,38 +12,32 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.inventory.ItemStack;
 
 @RequiredArgsConstructor
-public abstract class ContentProvider<L, K, V, T, U>
-    implements IContentProvider<L, K, V, T, U> {
+public abstract class ContentProvider<L, K, V, T, U> {
 
   protected final StockMarket stockMarket;
   protected final Settings settings;
 
-  @Override
   public Map<K, V> getContent(L lookup) {
     return Collections.emptyMap();
   }
 
-  @Override
   public Map<T, U> applyTransformations(Map<K, V> data) {
     return Collections.emptyMap();
   }
 
-  @Override
   public Map<String, Object> getExtraData(L lookup) {
     return Collections.emptyMap();
   }
 
-  @Override
   public ItemStack getContentStack(ItemStack baseStack, int position, T key, U value) {
     return null;
   }
 
-  @Override
   public ItemStack getExtraItem(ItemStack baseStack, Map<String, Object> extraData) {
     return baseStack;
   }
 
-  protected String formatInstant(Instant instant) {
+  protected final String formatInstant(Instant instant) {
     return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
         .withLocale(settings.getLocale())
         .withZone(ZoneId.systemDefault())
