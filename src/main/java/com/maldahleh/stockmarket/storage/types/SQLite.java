081004295c4965c3e47bb9404aea29907a893910
Mohammed Al-Dahleh <maldahleh@gmail.com>
2021-11-21 12:53:57 -0500

    Uses transaction builder for SQLite

--- src/main/java/com/maldahleh/stockmarket/storage/types/SQLite.java
+++ src/main/java/com/maldahleh/stockmarket/storage/types/SQLite.java
@@ -54,24 +54,22 @@ public class SQLite extends Storage {
       throws SQLException {
     BigDecimal earnings = null;
 
-    String earningsString = resultSet.getString(9);
+    String earningsString = resultSet.getString("earnings");
     if (earningsString != null) {
       earnings = new BigDecimal(earningsString);
     }
 
-    return new Transaction(
-        resultSet.getInt(1),
-        UUID.fromString(resultSet.getString(2)),
-        TransactionType.valueOf(resultSet.getString(3)),
-        resultSet.getTimestamp(4).toInstant(),
-        resultSet.getString(5),
-        resultSet.getInt(6),
-        new BigDecimal(resultSet.getString(7)),
-        new BigDecimal(resultSet.getString(8)),
-        earnings,
-        null,
-        null,
-        resultSet.getBoolean(10)
-    );
+    return Transaction.builder()
+        .id(resultSet.getInt("id"))
+        .uuid(UUID.fromString(resultSet.getString("uuid")))
+        .transactionType(TransactionType.valueOf(resultSet.getString("type")))
+        .transactionDate(resultSet.getTimestamp("date").toInstant())
+        .symbol(resultSet.getString("symbol"))
+        .quantity(resultSet.getInt("quantity"))
+        .singlePrice(new BigDecimal(resultSet.getString("single_price")))
+        .brokerFee(new BigDecimal(resultSet.getString("broker_fee")))
+        .earnings(earnings)
+        .sold(resultSet.getBoolean("sold"))
+        .build();
   }
 }
